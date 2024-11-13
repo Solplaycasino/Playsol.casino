a1e92539e6849c51a3f6638f397837ebbca2330d
Solplaycasino <solbetscasino@gmail.com>
2024-11-13 05:43:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,8 @@ export default function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/:gameId" element={<Game />} />
         </Routes>
-        
+        <h2 style={{ textAlign: 'center' }}>Recent Plays</h2>
+        <RecentPlays />
       </MainWrapper>
       <Footer />
     </>
